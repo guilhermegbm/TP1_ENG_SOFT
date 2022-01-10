f5e0c66a366e1186154d6e3f87ba7ffc853229aa
guilhermegbm <guilhermebmendonca60@gmail.com>
2022-01-10 15:01:57 -0300

    ishop-web: Sidebar do menu principal finalizada

--- ishop-web/src/pages/Dashboard.vue
+++ ishop-web/src/pages/Dashboard.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
+    <!--Navbar fixa no topo da tela-->
     <b-navbar sticky toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">
+      <b-navbar-brand href="#" v-b-toggle.main-menu-sidebar>
         <font-awesome-icon icon="bars" fixed-width />
       </b-navbar-brand>
 
@@ -31,8 +32,50 @@
       </b-collapse>
     </b-navbar>
 
+    <!--Sidebar "Togleable" lateral com menu de opções-->
+
+    <b-sidebar id="main-menu-sidebar" title="Menu" shadow>
+      <div class="px-3 py-2">
+        <b-row class="dashboard-item">
+          <b-col cols="12">Dashboard</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Produtos</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Histórico de Compras</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Meu Perfil</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Segurança</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Privacidade</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Dúvidas</b-col>
+        </b-row>
+        <b-row class="dashboard-item">
+          <b-col cols="12">Fale conosco</b-col>
+        </b-row>
+      </div>
+
+      <template #footer>
+        <hr style="margin: 0px" />
+        <div class="d-flex align-items-center px-3 py-2">
+          <font-awesome-icon style="margin-left: 90px" icon="info-circle" fixed-width size="lg" />
+          <font-awesome-icon style="margin-left: 10px" icon="file-signature" fixed-width size="lg" />
+          <font-awesome-icon style="margin-left: 8px" icon="star" fixed-width size="lg" />
+        </div>
+      </template>
+    </b-sidebar>
+
     <b-row class="header">
-      <b-col cols="12">Teste</b-col>
+      <b-col cols="12">
+        <b-button v-b-toggle.main-menu-sidebar>Toggle Sidebar</b-button>
+      </b-col>
     </b-row>
 
     <b-row class="header">
